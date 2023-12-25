2844469b56c5a40300cd753a515190a6cf056b3d
ZaneAyris <108333278+ZaneAyris@users.noreply.github.com>
2023-12-25 18:15:33 +1300

    Basic calculator for BMR

---
+++ models/calculator.py
@@ -0,0 +1,13 @@
+from person import Person
+
+def calculateBMR(person):
+        #Mifflin-St Jeor equation
+        baseBMR = 10 * person.weight + 6.25 * person.height - 5 * person.age + 5
+        match person.gender:
+            case "M":
+                return baseBMR + 5
+            case "F":
+                return baseBMR - 161
+            case _:
+                raise ValueError("Person does not have gender") 
+
